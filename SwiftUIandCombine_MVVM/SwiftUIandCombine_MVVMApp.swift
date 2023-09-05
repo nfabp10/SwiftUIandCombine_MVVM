73f8a7e27c8740c73adcb48824c38f9525b3c9e9
npfabp10 <npfabp10@gmail.com>
2023-09-05 19:58:24 -0300

    Initial Commit

---
+++ SwiftUIandCombine_MVVM/SwiftUIandCombine_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIandCombine_MVVMApp.swift
+//  SwiftUIandCombine_MVVM
+//
+//  Created by Nfabp10 on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIandCombine_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
